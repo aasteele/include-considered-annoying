fa5ce997509beb4066d181b4e9f19adb2a33b724
Aaron Steele <aasteele@cisco.com>
2019-08-21 16:45:20 -0700

    First working version of pre-specified config files and values
    Signed-off-by: Aaron Steele <aasteele@cisco.com>

--- ica.js
+++ ica.js
@@ -18,20 +18,111 @@ var vmlc_data = [];
 var clicked_element = [];
 var link = null;
 var node = [];
-var vmlc_d = null;
 
-var vmlc = d3.json("jsoninfo.json").then(function(d) {
-    vmlc_d = d; // Save the data for recalling in the future if needed
-    handleDataLoad(d);
+var vmlc_d = null;
+var vmlc = null;
+var configs = null;
+
+// First thing: read the available configurations
+var configs = d3.json("configurations.json").then(function(d) {
+    if(fileExists("jsoninfo.json")) {
+        startLoadData();
+    }
+    else {
+        // Read the config list and create the DOM elements
+        configs = d["config-list"];
+
+        var button_space = document.getElementById("button-space");
+        for(var i = 0; i < configs.length; ++i) {
+            var btn = document.createElement("input");
+            btn.setAttribute("type", "radio");
+            btn.setAttribute("name", "default_conf");
+            btn.setAttribute("id", "dc_" + configs[i]["name"]);
+            btn.setAttribute("value", i);
+            if(i == 0)
+                btn.setAttribute("checked", "true");
+
+            var lbl = document.createElement("label");
+            lbl.setAttribute("for", "dc_" + configs[i]["name"]);
+            lbl.innerText = configs[i]["name"];
+
+            button_space.appendChild(btn);
+            button_space.appendChild(lbl);
+            button_space.appendChild(document.createElement("br"));
+        }
+    }
 });
+
+d3.select("input[id=recalculate]").on("click", function() {
+    updateSizes();
+
+    // Do the actual reloading
+    handleDataLoad(vmlc_d);
+})
+// Set default values for width and height
+d3.select("input[id=tree-size-width]").attr("value", width);
+d3.select("input[id=tree-size-height]").attr("value", height);
+
+d3.select("input[id=load-configuration]").on("click", function() {
+    startLoadData();
+});
+
+function startLoadData() {
+    // Check if we have configs, or if we should only handle base jsoninfo.json
+    if(configs == null) {
+        vmlc = d3.json("jsoninfo.json").then(function(d) {
+            vmlc_d = d; // Save the data for recalling in the future if needed
+            handleDataLoad(d);
+        });
+    }
+    else {
+        // Get checked radio button value
+        var selected_id = d3.select('input[name="default_conf"]:checked').node().value;
+        var c = configs[selected_id];
+
+        d3.select("input[id=tree-size-width]").attr("value", c["default-values"]["tree-size"][0]);
+        d3.select("input[id=tree-size-height]").attr("value", c["default-values"]["tree-size"][1]);
+        d3.select("input[id=tree-sep-sibling]").attr("value", c["default-values"]["separation"][0]);
+        d3.select("input[id=tree-sep-not-sibling]").attr("value", c["default-values"]["separation"][1]);
+        d3.select("input[id=font-size]").attr("value", c["default-values"]["font-size"]);
+        updateSizes();
+        
+        vmlc = d3.json(c["file"]).then(function(d) {
+            vmlc_d = d; // Save the data for recalling in the future if needed
+            handleDataLoad(d);
+        });
+
+    }
+
+}
+
+function updateSizes() {
+    // Save these for length
+    tree_height         = parseInt(d3.select("input[id=tree-size-height]").property("value"));
+    tree_width          = parseInt(d3.select("input[id=tree-size-width]").property("value"));
+    tree_sibling        = parseFloat(d3.select("input[id=tree-sep-sibling").property("value"));
+    tree_not_sibling    = parseFloat(d3.select("input[id=tree-sep-not-sibling").property("value"));
+
+    // Update with the new sizes
+    tree
+        .size([tree_height - 100, tree_width - 160])
+        .separation((a, b) => { return a.parent == b.parent ? tree_sibling : tree_not_sibling; });
+
+    d3.select("svg").style("width", tree_width + "px");
+    d3.select("svg").style("height", tree_height + "px");
+}
 
 // ****** Handlers ******
 // When the data is loaded from the file, or everything needs to be redrawn for some reason
 function handleDataLoad(d) {
+    // Reload defaults for clean reload
     vmlc_data = [];
     clicked_element = [];
     link = null;
     node = [];
+
+    d3.selectAll("g > *").remove(); // Delete all the data elements, but keep g, which is the canvas element
+    d3.selectAll("button.collapsible").style("opacity", "1");
 
     // Format the data into a hierarchical structure
     tagpaths = d["tagpaths"]
@@ -90,78 +181,47 @@ function handleDataLoad(d) {
 
 // Create Event Handlers for mouse
 function handleMouseOverText(d, i) {
-
     // When hovering over a text element, highlight that element and all parents, and dim everything else
+    // This isn't a bad idea, but it's very slow when  there are a lot of elements
     //d3.selectAll("text").attr("fill", "grey");
 
-    setPathData(d, color_hover);
+    setIDPathData(d3.select(this).attr("id"), color_hover);
 }
 
 function handleMouseOutText(d, i) {
-
-    setPathData(d, color_default);
+    setIDPathData(d3.select(this).attr("id"), color_default);
 
     if(clicked_element != null) { // Something is clicked
-        // Check if that is what we're leaving now
+        // Re-apply whatever colors are supposed to be applied on the clicked elements
         for(var i = 0; i < clicked_element.length; i++) {
-            setStrPathData(clicked_element[i], color_click_scale(i));
+            setIDPathData(clicked_element[i], color_click_scale(i));
         }
     }
 }
 
 function handleMouseClickText(d, i) {
     // If we click on an element, set the path to a different color
-    setPathData(d, color_click_scale(clicked_element.length));
-    // Then save what we clicked
+    setIDPathData(d3.select(this).attr("id"), color_click_scale(clicked_element.length));
+    // Save what we clicked
     clicked_element.push(d3.select(this).attr("id"));
 }
 
-
-d3.select("input[id=recalculate]").on("click", function() {
-    d3.selectAll("g > *").remove(); // Delete all the data elements, but keep g, which is the canvas element
-    // Save these for length
-    tree_height = parseInt(d3.select("input[id=tree-size-height]").property("value"));
-    tree_width = parseInt(d3.select("input[id=tree-size-width]").property("value"));
-    tree_sibling = parseFloat(d3.select("input[id=tree-sep-sibling").property("value"));
-    tree_not_sibling = parseFloat(d3.select("input[id=tree-sep-not-sibling").property("value"));
-
-    // Update with the new sizes
-    tree.size([tree_height - 100, tree_width - 160])
-        .separation((a, b) => { return a.parent == b.parent ? tree_sibling : tree_not_sibling; });
-
-    d3.select("svg").style("width", tree_width + "px");
-    d3.select("svg").style("height", tree_height + "px");
-
-    // Do the actual reloading
-    handleDataLoad(vmlc_d);
-})
-d3.select("input[id=tree-size-width]").attr("value", width);
-d3.select("input[id=tree-size-height]").attr("value", height);
-
 // ****** Generics ******
-function setStrPathData(node_id, data) {
+function setIDPathData(node_id, data) {
+    // Applies to changes to a child and all of it's parents quickly
     // Passing in an ID directly as a string
+
+    // TODO: Add other options than just fill color
     name_split = node_id.split(".");
     for(i = 0; i < name_split.length; i++) {
         cur_elem = name_split.slice(0, i+1).join(".");
         elem = d3.select("text[id='" + cur_elem + "']").attr("fill", data);
     }
 }
-function setPathData(node, data) {
-    // Applies changes to a child and all of it's parents in linear time
-    // TODO: Add other options than just fill color
 
-    name_path = getNamePath(node);
-    name_split = name_path.split(".");
-    for(i = 0; i < name_split.length; i++) {
-        cur_elem = name_split.slice(0, i+1).join(".");
-        elem = d3.select("text[id='" + cur_elem + "']").attr("fill", data);
-    }
-}
 function getNamePath(node) {
     return getNameStack(node).reverse().join(".");
 }
-
 function getNameStack(node) {
     // Construct the stack with the hierarchy of the currently selected element
     name_stack = [];
@@ -216,4 +276,13 @@ function handle_path_arr(paths)  {
         }
         // If we *did* find it, we're done with this path
     });
+}
+
+// From https://stackoverflow.com/questions/15054182/javascript-check-if-file-exists
+function fileExists(url)
+{
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
 }
