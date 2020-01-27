3d16583e78218177325be2641cfbef53b15da5f7
Aaron Steele <aasteele@cisco.com>
2020-01-27 12:42:26 -0800

    Optional feature to include folders to search for yang files

--- README.md
+++ README.md
@@ -7,11 +7,20 @@ Go to the GitHub Pages Deployment: https://aasteele.github.io/include-considered
 
 ### Offline
 1. Clone the repository
-2. Run `sh run.sh`
+1. Run `sh run.sh`
 
 To run the program with a custom YANG file structure, run `sh run.sh ../path/to/yang/file.yang`  
 Ensure file.yang is a `module` and not a `submodule`, otherwise Yanger will produce no output.
 
+If the YANG files are in different directories, these can be specified in two ways:
+
+1. Adding all desired directories after the `path/to/yang/file.yang`
+
+   `sh run.sh ../path/to/yang/file.yang ~/Documents/project_1/yang1 ~/Documents/project_1/yangfolder2`
+1.  Specifying the recursive option after the `path/to/yang/file.yang`
+
+    `sh run.sh ../path/to/yang/file.yang -r ~/Documents/project_1/`
+   
 Note: Yanger is only required to parse new YANG files.
 
 
--- run.sh
+++ run.sh
@@ -2,13 +2,34 @@
 # Handle ctrl-c for killing process
 trap "kill 0" EXIT
 
-if [ ! -z "$1" ]
-then
+if [[ ! -z "$1" ]]; then
   # Run yanger with the input file $1
   dir=$(dirname $1)
   fname=$(basename $1 .yang)
+  path=${dir}
 
-  yanger -p $dir -f jsoninfo $1 > jsoninfo.json
+  # Make sure we're messing with directories at all
+  if [[ ! -z "$2" ]]; then
+    # If we want to recursively include every single folder in the third argument
+    if [[ $2 = "-r" ]]; then
+      du_dir=$(du $3)
+      for i in ${du_dir}; do
+        # Check if it's a number, if so ignore it
+        re='^[0-9]+$'
+        if ! [[ ${i} =~ $re ]]; then
+          path="${path}:${i}"
+        fi
+      done
+    else
+      # Just include the directories that the user lists
+      # Get all args except the first
+      for i in "${@:2}"; do
+         path="${path}:${i}"
+      done
+    fi
+  fi
+
+  yanger -p ${path} -f jsoninfo $1 > jsoninfo.json
 fi
 
 python3 -m http.server 8000 &
